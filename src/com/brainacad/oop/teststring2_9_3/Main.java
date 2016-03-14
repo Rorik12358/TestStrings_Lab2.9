975f044f49874268a2154a832aeafc747d7df9cd
Roman Mazurov <mazurov.rn@gmail.com>
2016-03-15 00:25:22 +0200

    New solution

--- src/com/brainacad/oop/teststring2_9_3/Main.java
+++ src/com/brainacad/oop/teststring2_9_3/Main.java
@@ -8,27 +8,29 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Проверяю работу метода
-        System.out.println(uniqueChars("agdgash"));
+        System.out.println(uniqueChars("ssgppk"));
 
         System.out.println(Arrays.toString(uniqueChars("Using methods of class String")));
     }
 
-    public static char[] uniqueChars(String s) {
-        // используем StringBuilder, что бы не перегружать память (для цыклов стринги лучше не использовать)
-        StringBuilder stringForArr = new StringBuilder();
-        //Проверяеем есть ли еще один символ в строке как і-тый символ
-        for (int i = 0; i < s.length(); i++) {
-            if (i == 0) {
-                //используем toLowerCase метод ,что бы не зависить от регистра
-                if (s.toLowerCase().substring(i + 1).indexOf(s.toLowerCase().charAt(i)) < 0) { //bad decision. A lot of memory usage. Not clear logic.
-                    stringForArr.append(s.toLowerCase().charAt(i));                           //Try to use more simple way
-                }
-            } else {
-                if (s.toLowerCase().substring(i + 1).indexOf(s.toLowerCase().charAt(i)) < 0 && s.toLowerCase().substring(0, i).indexOf(s.toLowerCase().charAt(i)) < 0) {
-                    stringForArr.append(s.charAt(i));
+    public static char[] uniqueChars(String input) {
+        char[] chrs = input.toCharArray();
+        char[] forSolution = new char[input.length()];
+        int index = 0;
+        for (char c : chrs) {
+            int coinsedence = 0;
+            for (char ch : chrs) {
+                if (c == ch) {
+                    coinsedence++;
                 }
             }
+            if (coinsedence == 1) {
+                forSolution[index] = c;
+                index++;
+            }
         }
-        return stringForArr.toString().toCharArray();
+        return Arrays.copyOfRange(forSolution, 0, index);
     }
+
+
 }
