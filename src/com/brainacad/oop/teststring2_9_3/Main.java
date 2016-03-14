eb58f55571a1ef4a73c0ca40f40d720e6f56ea3a
jref <aleksandrnvk@ukr.net>
2016-03-14 17:50:55 +0200

    Update Main.java

--- src/com/brainacad/oop/teststring2_9_3/Main.java
+++ src/com/brainacad/oop/teststring2_9_3/Main.java
@@ -20,8 +20,8 @@ public class Main {
         for (int i = 0; i < s.length(); i++) {
             if (i == 0) {
                 //используем toLowerCase метод ,что бы не зависить от регистра
-                if (s.toLowerCase().substring(i + 1).indexOf(s.toLowerCase().charAt(i)) < 0) {
-                    stringForArr.append(s.toLowerCase().charAt(i));
+                if (s.toLowerCase().substring(i + 1).indexOf(s.toLowerCase().charAt(i)) < 0) { //bad decision. A lot of memory usage. Not clear logic.
+                    stringForArr.append(s.toLowerCase().charAt(i));                           //Try to use more simple way
                 }
             } else {
                 if (s.toLowerCase().substring(i + 1).indexOf(s.toLowerCase().charAt(i)) < 0 && s.toLowerCase().substring(0, i).indexOf(s.toLowerCase().charAt(i)) < 0) {
